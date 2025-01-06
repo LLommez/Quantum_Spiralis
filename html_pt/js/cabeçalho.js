c6e281bd08b6a6d30287236b93b9f4a3c5cf9017
Lucca Lommêz <profluccalommez@gmail.com>
2025-01-06 13:55:44 -0300

    att

--- html_pt/js/cabeçalho.js
+++ html_pt/js/cabeçalho.js
@@ -1,16 +1,18 @@
 let prevScrollPos = window.pageYOffset; // Armazena a posição de rolagem anterior
 const header = document.querySelector("header"); // Seleciona o cabeçalho
+const headerHeight = header.offsetHeight; // Altura do cabeçalho
 
 window.addEventListener("scroll", () => {
     const currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) {
-        // Rolagem para cima: mostra o cabeçalho
-        header.style.top = "0";
+    // Exibe o cabeçalho somente se o usuário estiver no topo da página
+    if (currentScrollPos <= headerHeight) {
+        header.style.top = "0";  // Mostra o cabeçalho no topo
     } else {
-        // Rolagem para baixo: esconde o cabeçalho completamente
-        header.style.top = `-${header.offsetHeight}px`;
+        // Caso contrário, mantém o cabeçalho escondido
+        header.style.top = `-${headerHeight}px`;
     }
 
     prevScrollPos = currentScrollPos;
 });
+
